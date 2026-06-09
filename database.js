17337f6631795f15fb79598313570815edc5f3bf
Andres Flores <andesbyte2026@gmail.com>
2026-06-09 15:22:31 -0500

    usar variables railway

--- database.js
+++ database.js
@@ -1,16 +1,20 @@
 const mysql = require("mysql2");
 
-const conexion = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const conexion = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT)
 });
 
-console.log("✅ MYSQL CONFIGURADO PARA RENDER");
+conexion.connect((error) => {
+  if (error) {
+    console.log("❌ ERROR MYSQL");
+    console.log(error);
+  } else {
+    console.log("✅ MYSQL CONECTADO RAILWAY");
+  }
+});
 
 module.exports = conexion;
